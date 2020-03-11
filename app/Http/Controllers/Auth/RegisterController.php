149039f4cd89ff22f5a48532eb344b100983c642
DESKTOP-5UQC327 <reckyanderson@gmail.com>
2020-03-11 15:43:46 +0700

    Updating a lot of improvements for adjusting the model and table

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Group;
+use App\Contest;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -40,8 +42,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'username' => ['required', 'string', 'max:255', 'unique:users'],
+            'district' => ['string', 'max:255'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'birthdate' => ['required'],
             'gender' => ['required', 'in:putera,puteri'],
@@ -60,14 +62,32 @@ class RegisterController extends Controller
 
         $user = new User;
         $user->name = $data['name'];
-        $user->email = $data['email'];
         $user->username = $data['username'];
+        $user->district = $data['district'];
         $user->password = Hash::make($data['password']);
         $user->birthdate = $data['birthdate'];
         $user->gender = $data['gender'];
         $user->is_admin = false;
         $user->participant_no = $latest_participant_no == null ? ($data['gender'] == 'putera' ? 1 : 2) : ($latest_participant_no->only('participant_no')['participant_no'] + 2);
-        $user->contest()->associate($data['golongan-lomba']);
+
+        $contest = Contest::find($data['golongan-lomba']);
+        if($contest->name == 'Cabang Fahm Al Qur\'an Umur maksimal 18 tahun 11 bulan 29 hari' || $contest->name == 'Cabang Syarh Al Qur\'an Umur maksimal 18 tahun 11 bulan 29 hari'){
+            if (array_key_exists('group-name',$data)) {
+            $group = new Group;
+            $group->name = $data['group-name'];
+            $group->description = $data['group-description'];
+            $group->types = $data['gender'];
+            $group->contest()->associate($data['golongan-lomba']);
+            $group->save();
+            $user->group()->associate($group);
+            }else {
+                $user->group()->associate($data['join-group']);
+            }
+
+        }else {
+            $user->contest()->associate($data['golongan-lomba']);
+        }
+
         $user->save();
 
         return $user;
