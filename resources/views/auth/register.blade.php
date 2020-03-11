8d8b70f56feeb753e86cffda6a344c01d9585b91
DESKTOP-5UQC327 <reckyanderson@gmail.com>
2020-03-11 15:46:09 +0700

    Improvements on register blade :D

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,14 +27,15 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Alamat Email') }}</label>
+                            <label for="username"
+                                class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="username" type="username"
+                                    class="form-control @error('username') is-invalid @enderror" name="username"
+                                    value="{{ old('username') }}" required autocomplete="username">
 
-                                @error('email')
+                                @error('username')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
@@ -69,6 +70,22 @@
                             </div>
                         </div>
                         <div class="form-group row">
+                            <label for="district"
+                                class="col-md-4 col-form-label text-md-right">{{ __('Kecamatan') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="district" type="district"
+                                    class="form-control @error('district') is-invalid @enderror" name="district"
+                                    required autocomplete="new-district">
+
+                                @error('district')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
                             <label for="birthDate"
                                 class="col-md-4 col-form-label text-md-right">{{ __('Masukkan Tgl Lahir') }}</label>
 
@@ -76,38 +93,8 @@
                                 <input id="birthDate" name="birthdate" width="276">
                             </div>
                         </div>
-                        <fieldset class="form-group">
-                            <div class="row">
-                                <legend class="col-form-label col-md-2 offset-md-2 col-sm-2 pt-0">Jenis Kelamin</legend>
-                                <div class="col-sm-10 col-md-6">
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="gender" id="genderRadio1"
-                                            value="putera" checked>
-                                        <label class="form-check-label" for="genderRadio1">
-                                            Putera
-                                        </label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="gender" id="genderRadio2"
-                                            value="puteri">
-                                        <label class="form-check-label" for="genderRadio2">
-                                            Puteri
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                        </fieldset>
-
-                        <div class="form-group row">
-                            <label for="golongan-lomba" class="col-md-4 col-form-label text-md-right">Pilih Golongan
-                                Lomba</label>
-                            <div class="col-md-6">
-                                <select name="golongan-lomba" id="golonganlomba" class="custom-select">
-
-                                </select>
-                            </div>
-
-                        </div>
+                        <part-register :gol-putera='@json($putera)' :searching='@json(route('search'))' :gol-puteri='@json($puteri)' >
+                        </part-register>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
@@ -124,104 +111,71 @@
 </div>
 @endsection
 
+
 @section('scripts')
 
 
 <script>
-    const mainSelect = document.getElementById('golonganlomba');
-    let glomba = @json(App\ Contest::all());
-    $(document).ready(function () {
-        glomba.forEach(element => {
-            if (element['types'] == 'putera') {
-                var option = document.createElement("option");
-                option.text = element.name;
-                option.value = element.id;
-                mainSelect.appendChild(option);
-            }
-        });
-    });
-    $("#myForm input[type=radio]").on("change", function () {
-        if (this.checked) {
-            mainSelect.innerHTML = '';
-            glomba.forEach(element => {
-                if (element['types'] == this.value) {
-                    var option = document.createElement("option");
-                    option.text = element.name;
-                    option.value = element.id;
-                    mainSelect.appendChild(option);
-                }
-            });
-        }
-    });
-
-</script>
-
-
-
-
-<script>
     gj.dialog.messages['id-id'] = {
-        Close: 'Tutup',
-        DefaultTitle: 'Dialog'
-    };
-    gj.grid.messages['id-id'] = {
-        First: 'Pertama',
-        Previous: 'Sebelumnya',
-        Next: 'Selanjutnya',
-        Last: 'Terakhir',
-        Page: 'Halaman',
-        FirstPageTooltip: 'Hal Pertama',
-        PreviousPageTooltip: 'Hal Sebelumnya',
-        NextPageTooltip: 'Hal Selanjutnya',
-        LastPageTooltip: 'Hal Terakhir',
-        Refresh: 'Muat Ulang',
-        Of: 'dari',
-        DisplayingRecords: 'Menampilkan rekaman',
-        RowsPerPage: 'Baris per halaman:',
-        Edit: 'Ubah',
-        Delete: 'Hapus',
-        Update: 'Perbarui',
-        Cancel: 'Batal',
-        NoRecordsFound: 'Tidak ada data.',
-        Loading: 'Menunggu...'
-    };
-    gj.editor.messages['id-id'] = {
-        bold: 'Bold',
-        italic: 'Italic',
-        strikethrough: 'Strikethrough',
-        underline: 'Underline',
-        listBulleted: 'List Bulleted',
-        listNumbered: 'List Numbered',
-        indentDecrease: 'Indent Decrease',
-        indentIncrease: 'Indent Increase',
-        alignLeft: 'Align Left',
-        alignCenter: 'Align Center',
-        alignRight: 'Align Right',
-        alignJustify: 'Align Justify',
-        undo: 'Undo',
-        redo: 'Redo'
-    };
-    gj.core.messages['id-id'] = {
-        monthNames: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September',
-            'October', 'November', 'December'
-        ],
-        monthShortNames: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
-        weekDaysMin: ['M', 'N', 'A', 'R', 'K', 'J', 'U'],
-        weekDaysShort: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
-        weekDays: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
-        am: 'AM',
-        pm: 'PM',
-        ok: 'OK',
-        cancel: 'Batalkan',
-        titleFormat: 'mmmm yyyy'
-    };
-    $('#birthDate').datepicker({
-        locale: 'id-id',
-        uiLibrary: 'bootstrap4',
-        format: 'yyyy-mm-dd'
-    });
-
+    Close: 'Tutup',
+    DefaultTitle: 'Dialog'
+  };
+  gj.grid.messages['id-id'] = {
+    First: 'Pertama',
+    Previous: 'Sebelumnya',
+    Next: 'Selanjutnya',
+    Last: 'Terakhir',
+    Page: 'Halaman',
+    FirstPageTooltip: 'Hal Pertama',
+    PreviousPageTooltip: 'Hal Sebelumnya',
+    NextPageTooltip: 'Hal Selanjutnya',
+    LastPageTooltip: 'Hal Terakhir',
+    Refresh: 'Muat Ulang',
+    Of: 'dari',
+    DisplayingRecords: 'Menampilkan rekaman',
+    RowsPerPage: 'Baris per halaman:',
+    Edit: 'Ubah',
+    Delete: 'Hapus',
+    Update: 'Perbarui',
+    Cancel: 'Batal',
+    NoRecordsFound: 'Tidak ada data.',
+    Loading: 'Menunggu...'
+  };
+  gj.editor.messages['id-id'] = {
+    bold: 'Bold',
+    italic: 'Italic',
+    strikethrough: 'Strikethrough',
+    underline: 'Underline',
+    listBulleted: 'List Bulleted',
+    listNumbered: 'List Numbered',
+    indentDecrease: 'Indent Decrease',
+    indentIncrease: 'Indent Increase',
+    alignLeft: 'Align Left',
+    alignCenter: 'Align Center',
+    alignRight: 'Align Right',
+    alignJustify: 'Align Justify',
+    undo: 'Undo',
+    redo: 'Redo'
+  };
+  gj.core.messages['id-id'] = {
+    monthNames: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September',
+      'October', 'November', 'December'
+    ],
+    monthShortNames: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
+    weekDaysMin: ['M', 'N', 'A', 'R', 'K', 'J', 'U'],
+    weekDaysShort: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+    weekDays: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+    am: 'AM',
+    pm: 'PM',
+    ok: 'OK',
+    cancel: 'Batalkan',
+    titleFormat: 'mmmm yyyy'
+  };
+  $('#birthDate').datepicker({
+    locale: 'id-id',
+    uiLibrary: 'bootstrap4',
+    format: 'yyyy-mm-dd'
+  });
 </script>
-
 
 @endsection
